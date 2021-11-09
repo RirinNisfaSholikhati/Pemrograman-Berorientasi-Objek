5b48a8d875e386d25672182adaabb8d97a2ccc96
RirinNisfaSholikhati <71623155+RirinNisfaSholikhati@users.noreply.github.com>
2021-11-09 23:04:39 +0700

    PBO-11

---
+++ SortingProject/src/sortingproject/ArrayProcessing.java
@@ -0,0 +1,89 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sortingproject;
+
+/**
+ *
+ * @author ASUS
+ */
+import java.util.Scanner;
+import java.util.Arrays;
+
+
+public class ArrayProcessing {
+    // data array 
+    int[] dataBil = new int[100];
+    
+    //jumlah data 
+    int n;
+    private int temp;
+    
+    void input(){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukkan banyaknya data (n): ");
+        this.n = input.nextInt();
+        for(int i=0; i<n; i++){
+            System.out.println("Masukkan bil ke-" +(i+1)+": ");
+            this.dataBil[i] = input.nextInt();
+        }
+    }
+    
+    double hitungRerata(int[] data){
+        double sum = 0;
+        for(int i=0; i<this.n; i++){
+            sum += data[i];
+        }
+        double rerata = sum/this.n;
+        return rerata;
+    }
+    
+    //PROJECT 1 MinMax Project
+    //Method mencari nilai Minimum data array
+    int cariMin(int[] dataBil){
+        int min = dataBil[0];
+        for (int i=1; i<this.n; i++){
+            if (min > dataBil[i]){
+                min = dataBil[i];
+            }
+        }
+        return min;
+    }
+    
+    //Method mencari nilai Maksimum data array
+    int cariMax(int[] dataBil){
+        int max = dataBil[0];
+        for (int i=1; i<this.n; i++){
+            if (max < dataBil[i]){
+                max = dataBil[i];
+            }
+        }
+        return max;
+    }
+    
+    //Mengurutkan data aarray secara ascending
+   int[] urutkan(int[] dataBil){
+        int[] hasil = new int[this.n];
+        for (int a = 0; a < this.n; a++){
+            hasil[a] = dataBil[a];
+        }
+        for (int i = 0; i < dataBil.length; i++){
+            for (int j = 0; j < hasil.length-1; j++){
+                if (hasil[j] > hasil[j+1]){
+                    int baru = hasil[j];
+                    hasil[j] = hasil[j+1];
+                    hasil[j+1] = baru;
+                }
+            }
+        }
+        return hasil;
+    }
+    void output(){
+        System.out.println("Reratanya: "+this.hitungRerata(this.dataBil));
+        System.out.println("Nilai Minimumnya: " + this.cariMin(dataBil));
+        System.out.println("Nilai Maksimumnya: " + this.cariMax(dataBil));
+        System.out.println("Data setelah di urutkan secara Ascending: " + Arrays.toString(this.urutkan(dataBil)));
+    }
+}
